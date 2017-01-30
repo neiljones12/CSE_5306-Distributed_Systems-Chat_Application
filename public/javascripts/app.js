aec902f2a2d2cca966151a8fc1ced4c522f3a840
Neil Jones <neil.j.neil@gmail.com>
2017-01-30 06:29:50 -0600

    Chatting functionality

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,4 @@
-const app = angular.module('app', ['ngRoute', 'ngStorage']); 
+const app = angular.module('app', ['ngStorage']);
 
 app.factory('socket', function ($rootScope) {
     const socket = io.connect();
@@ -26,6 +26,13 @@ app.factory('socket', function ($rootScope) {
 
 app.controller('Controller', function ($scope, $http, $localStorage, $location, socket) {
     $scope.init = function () {
+        $localStorage["users"] = null;
+        $scope.socketId = null;
+        $scope.selectedUser = null;
+        $scope.messages = [];
+        $scope.msgData = null;
+        $scope.userList = [];
+
         $scope.login = true;
         $localStorage["currentUser"] = null;
         $scope.username = "";
@@ -35,9 +42,12 @@ app.controller('Controller', function ($scope, $http, $localStorage, $location, 
     };
 
     $scope.visibleLogin = function () {
-        $scope.check($scope.username, true)
-        var user = { id: $localStorage["users"].length + 1, name: $scope.username, visible: true };
-        $scope.register(user); 
+        if ($scope.username != "")
+        {
+            $scope.check($scope.username, true)
+            var user = { id: $localStorage["users"].length + 1, name: $scope.username, visible: true };
+            $scope.register(user);
+        } 
     };
 
     $scope.invisibleLogin = function () {
@@ -57,9 +67,10 @@ app.controller('Controller', function ($scope, $http, $localStorage, $location, 
     };
 
     $scope.register = function (user) {
+        //console.log(user.name);
         var users = JSON.parse($localStorage["users"]);
         users.push(user);
-        $localStorage["users"] = JSON.stringify(users);
+        $localStorage["users"] = JSON.stringify(users); 
         $scope.redirect(user.name);
     };
 
@@ -68,9 +79,47 @@ app.controller('Controller', function ($scope, $http, $localStorage, $location, 
         $scope.login = true;
     };
 
-    $scope.redirect = function (username) {
+    $scope.redirect = function (username) { 
         $localStorage["currentUser"] = username;
         $scope.login = false;
         $scope.username = $localStorage["currentUser"];
+        socket.emit('username', $localStorage["currentUser"]);
     };
+
+    $scope.seletedUser = (selectedUser) => {
+        selectedUser === $scope.socketId ? alert("Can't message to yourself.") : $scope.selectedUser = selectedUser;
+    };
+
+
+    $scope.sendMsg = ($event) => {
+        const keyCode = $event.which || $event.keyCode;
+
+        if (keyCode === 13 && $scope.message !== null) {
+            socket.emit('getMsg', {
+                toid: $scope.selectedUser,
+                msg: $scope.message,
+                name: $scope.username
+            });
+            $scope.message = null;
+        }
+    };
+
+
+    //socket.emit('username', $scope.username);
+
+    socket.on('userList', (userList, socketId) => {
+        if ($scope.socketId === null) {
+            $scope.socketId = socketId;
+        }
+        $scope.userList = userList;
+    });
+
+
+    socket.on('exit', (userList) => {
+        $scope.userList = userList;
+    });
+
+    socket.on('sendMsg', (data) => {
+        $scope.messages.push(data);
+    });
 });
